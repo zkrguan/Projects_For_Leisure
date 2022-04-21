c9a804af1386686d161ce19c754a28a67137c37b
Zhaokai Guan <zguan25@myseneca.ca>
2022-04-20 21:10:05 -0400

    Made it flexible and fixed the bug when num of height is even.

--- Display_A_Diamond/Source.cpp
+++ Display_A_Diamond/Source.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int holesRangeleft = 14;
-	int holeRangeRight = 14;
-	for (int i = 0; i < 31; i++) {
+	char fillWith = '^';
+	int Hlength = 106;
+	(Hlength % 2) ? Hlength++ : Hlength;
+	int Vlength = Hlength;
+	int holesRangeleft = Vlength/2;
+	int holeRangeRight = holesRangeleft;
+	for (int i = 0; i < Vlength+2; i++) {
 		cout<<" ";
-		for (int j = 0; j < 30; j++){
-			cout<<((j > holesRangeleft&& j < holeRangeRight)? "*" : " ");
+		for (int j = 0; j < Hlength+1; j++){
+			cout<<((j > holesRangeleft&& j < holeRangeRight)? fillWith : ' ');
 		}
-		i <= 14 ? holeRangeRight++ : holeRangeRight--;
-		i <= 14 ? holesRangeleft-- : holesRangeleft++;
+		i <= Vlength / 2 ? holeRangeRight++ : holeRangeRight--;
+		i <= Vlength / 2 ? holesRangeleft-- : holesRangeleft++;
 		cout << "\n";
 	}
 	return 0;
